81942aa3b8e6c085d1d26fe1cde28f62bb5caeb0
smopper <smopperette@gmail.com>
2020-06-29 07:38:00 +0100

    Testing

---
+++ Assets/Tests/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PauseTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void PauseTestPasses()
+        {
+            var gamesession = new GameSession();
+            Assert.AreEqual(true, gamesession.gamePause());
+        }
+
+    }
+}
